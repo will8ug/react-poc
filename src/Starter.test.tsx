bf641d6ca0b6db39695b96fbd45b1c62d106e842
Will <yzj_000@163.com>
2026-05-10 21:02:42 +0800

    Add a starter page with Links

---
+++ src/Starter.test.tsx
@@ -0,0 +1,32 @@
+import {screen, render, fireEvent} from "@testing-library/react";
+
+import Starter from "./Starter.tsx";
+import {expect} from "vitest";
+
+describe('Starter', () => {
+    it('Should render navigation and home view by default', () => {
+        render(<Starter />);
+
+        expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+        expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
+        expect(screen.getByRole('link', { name: 'Contact' })).toBeInTheDocument();
+
+        expect(screen.getByRole('heading', { name: 'Home Page' })).toBeInTheDocument();
+    });
+
+    it('About link should work', () => {
+        render(<Starter />);
+
+        fireEvent.click(screen.getByRole('link', { name: 'About' }));
+
+        expect(screen.getByRole('heading', { name: 'About Page' })).toBeInTheDocument();
+    });
+
+    it('Contact link should work', () => {
+        render(<Starter />);
+
+        fireEvent.click(screen.getByRole('link', { name: 'Contact' }));
+
+        expect(screen.getByRole('heading', { name: 'Contact Page' })).toBeInTheDocument();
+    });
+});
---
+++ src/Starter.tsx
@@ -0,0 +1,31 @@
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
+
+export default function Starter() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
